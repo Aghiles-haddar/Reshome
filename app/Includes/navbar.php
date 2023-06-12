8c1c9646a2204e68d45b43dd7f5267fbba43970c
dwain93 <74243765+dwain93@users.noreply.github.com>
2023-06-12 18:40:27 +0200

    Ajout de la page de logement

--- app/Includes/navbar.php
+++ app/Includes/navbar.php
@@ -1,16 +1,17 @@
 <div class="container-navbar">
         <ul class="info-left">
-            <li class="info">ABOUT PROJECT</li>
-            <li class="info">CONSTRUCTION PROGRESS</li>
-            <li class="info">CONTACT</li>
+            <li class="info">Accueil</li>
+            <li class="info">Catalogue d'appartement</li>
+            <li class="info">Contact</li>
         </ul>
 
         <img class="img-nav" src="./Front/CSS/Assets/logo.svg" alt="">
 
         <ul class="info-right">
-            <li class="info">+33 6 32 01 01 93</li>
-            <li class="info li-border">SEARCH APPARTEMENT  <span class="img-li-border"><img src="../CSS/Assets/arrowdown.png" alt=""></span></li>
-            <li class="info">AVIS</li>
+            <li class="info li-border">SEARCH APPARTEMENT  <span class="img-li-border"><img src="./Front/CSS/Assets/arrowdown.png" alt=""></span></li>
+            <li class="info">Inscription</li>
+            <li class="info">Connexion</li>
         </ul>
 </div>
+
 
